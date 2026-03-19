77b6e9fa0d09870cece550b0622284347b34e3fb
TaffaByte <188018752+TaffaByte@users.noreply.github.com>
2026-03-19 11:45:40 -0700

    fixed addressdecoder

--- Lab08/sources_1/new/AddressDecoder.v
+++ Lab08/sources_1/new/AddressDecoder.v
@@ -1,18 +1,16 @@
 `timescale 1ns / 1ps
 
-module AddressDecoder(
-    input [1:0] address,
-    input writeData,
-    input readData,
-    output dataMemWrite,
-    output dataMemRead,
-    output LEDWrite,
-    output switchReadEnable
+module AddressDecoder (
+    input  [1:0] address,
+    input        writeData, 
+    input        readData,
+    output       dataMemWrite, 
+    output       dataMemRead, 
+    output       LEDWrite, 
+    output       switchReadEnable
 );
-
-    assign dataMemWrite      = (address == 2'b00) && writeData;
-    assign dataMemRead       = (address == 2'b00) && readData;
-    assign LEDWrite          = (address == 2'b01) && writeData;
-    assign switchReadEnable  = (address == 2'b10) && readData;
-
+    assign dataMemWrite     = (address == 2'b00) ? writeData : 1'b0;
+    assign dataMemRead      = (address == 2'b00) ? readData  : 1'b0;
+    assign LEDWrite         = (address == 2'b01) ? writeData : 1'b0;
+    assign switchReadEnable = (address == 2'b10) ? readData  : 1'b0;
 endmodule
